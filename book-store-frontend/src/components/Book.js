c6e7fcad13015aa351447c688020cb36d0fc9a2b
ADMIN <giang08t3@gmail.com>
2023-07-28 10:17:30 +0700

    Frontend: implement NumericFormat

--- book-store-frontend/src/components/Book.js
+++ book-store-frontend/src/components/Book.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NumericFormat } from "react-number-format";
 
 const Book = ({book}) => {
   return (
@@ -10,7 +11,9 @@ const Book = ({book}) => {
             <div className='text-sm text-gray-500'>{book.description}</div>
         </td>
         <td className='text-left px-6 py-4 whitespace-nowrap'>
-            <div className='text-sm text-gray-500'>{book.price}</div>
+            <div className='text-sm text-gray-500'>
+                <NumericFormat value={book.price} thousandSeparator=","></NumericFormat>
+            </div>
         </td>
         <td className='text-left px-6 py-4 whitespace-nowrap font-medium text-sm'>
             <a className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Edit</a>
